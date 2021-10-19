bde485c2d0f5adb14817292a4c3e839155040694
Alfredo Navas <elpuas@gmail.com>
2021-10-19 07:08:47 -0600

    feebee nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { request } from "../lib/datocms";
 import styles from '../styles/Home.module.css'
+import ReactMarkdown from 'react-markdown'
 
-export default function Home() {
+export default function Home({data}) {
+  const {
+    article:{
+      id,
+      slug,
+      title,
+      _createdAt,
+      _modelApiKey,
+      content,
+      featureImage
+    }
+  } = data
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,43 +26,11 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div id={id}>
+          <Image src={featureImage.url} width={800} height={400} alt={featureImage.alt} />
+          <h2>{title}</h2>
+          <p>{`Publish on: ${_createdAt}`}</p>
+          <ReactMarkdown>{content}</ReactMarkdown>
         </div>
       </main>
 
@@ -67,3 +49,27 @@ export default function Home() {
     </div>
   )
 }
+
+export async function getStaticProps() {
+  const data = await request({
+    query: ARTICLE_QUERY
+  });
+  return {
+    props: { data }
+  };
+}
+
+const ARTICLE_QUERY = `query ArticleQuery {
+  article {
+    id
+    slug
+    title
+    _createdAt
+    _modelApiKey
+    content
+    featureImage {
+      url
+      alt
+    }
+  }
+}`;
